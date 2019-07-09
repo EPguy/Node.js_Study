c6db773524563065ed1ebe7a4830a8d46c046ced
EPguy <36794920+EPguy@users.noreply.github.com>
2019-07-09 15:05:38 +0900

    update

--- test1/router/main.js
+++ test1/router/main.js
@@ -1,8 +1,12 @@
 module.exports = function(app, fs){
     app.get('/', function(req, res) {
+        var sess = req.session;
+
         res.render('index', {
             title: "My Hompage",
-            length: 5
+            length: 5,
+            name: sess.name,
+            username: sess.username
         })
     });
     app.get('/list', function(req, res) {
@@ -103,5 +107,48 @@ module.exports = function(app, fs){
             })
         })
 
+    })
+    app.get('/login/:username/:password', function(req, res) {
+        var sess;
+        sess = req.session;
+
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function(err,data) {
+            var users = JSON.parse(data);
+            console.log(users);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+            if(!users[username]) {
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            if(users[username]["password"] == password) {
+                result["success"] = 1;
+                sess.username = username;
+                sess.name = users[username]["name"];
+                res.json(result);
+            } else {
+                result["success"] = 0;
+                result["error"] = "incorrect";
+                res.json(result);
+            }
+        })
+    })
+    app.get('/logout', function(req, res) {
+        sess = req.session;
+        if(sess.username) {
+            req.session.destory(function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    res.redirect('/');
+                }
+            })
+        } else {
+            res.redirect('/');
+        }
     })
 }
